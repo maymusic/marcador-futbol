cdf345962cfa296db6bb520c37576087922f7ece
maymusic <maymusicmexico@gmail.com>
2026-06-21 16:05:58 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -11,22 +11,13 @@ app.use(express.static('public'));
 let gameState = {
     showScoreboard: true,
     mainColor: '#1a1a2e',
-    homeName: 'RAC',
-    homeFullName: 'RACING DE VERACRUZ',
-    awayName: 'VIS',
-    awayFullName: 'VISITANTE FC',
-    homeScore: 0,
-    awayScore: 0,
-    homeColor: '#0044cc',
-    awayColor: '#cc0000',
-    homeLogo: 'rac.png', 
-    awayLogo: 'default.png',
-    homeReds: 0,
-    awayReds: 0,
-    minutes: 0,
-    seconds: 0,
-    addedTime: 0,
-    isRunning: false
+    homeName: 'RAC', homeFullName: 'RACING DE VERACRUZ',
+    awayName: 'VIS', awayFullName: 'VISITANTE FC',
+    homeScore: 0, awayScore: 0,
+    homeColor: '#0044cc', awayColor: '#cc0000',
+    homeLogo: 'default.png', awayLogo: 'default.png',
+    homeReds: 0, awayReds: 0,
+    minutes: 0, seconds: 0, addedTime: 0, isRunning: false
 };
 
 let timerInterval;
@@ -36,10 +27,7 @@ function startTimer() {
         gameState.isRunning = true;
         timerInterval = setInterval(() => {
             gameState.seconds++;
-            if (gameState.seconds >= 60) {
-                gameState.seconds = 0;
-                gameState.minutes++;
-            }
+            if (gameState.seconds >= 60) { gameState.seconds = 0; gameState.minutes++; }
             io.emit('updateTimer', { minutes: gameState.minutes, seconds: gameState.seconds });
         }, 1000);
     }
@@ -55,65 +43,19 @@ io.on('connection', (socket) => {
     socket.emit('stateUpdate', gameState);
     socket.emit('updateTimer', { minutes: gameState.minutes, seconds: gameState.seconds });
 
-    socket.on('toggleScoreboard', (isVisible) => {
-        gameState.showScoreboard = isVisible;
-        io.emit('stateUpdate', gameState);
-    });
-
-    socket.on('updateScore', (data) => {
-        gameState.homeScore = data.homeScore;
-        gameState.awayScore = data.awayScore;
-        io.emit('stateUpdate', gameState);
-    });
-
-    socket.on('updateTeamInfo', (data) => {
-        gameState.mainColor = data.mainColor;
-        gameState.homeName = data.homeName;
-        gameState.homeFullName = data.homeFullName;
-        gameState.awayName = data.awayName;
-        gameState.awayFullName = data.awayFullName;
-        gameState.homeColor = data.homeColor;
-        gameState.awayColor = data.awayColor;
-        gameState.homeLogo = data.homeLogo;
-        gameState.awayLogo = data.awayLogo;
-        io.emit('stateUpdate', gameState);
-    });
-
-    socket.on('updateAddedTime', (time) => {
-        gameState.addedTime = time;
-        io.emit('stateUpdate', gameState);
-    });
-
-    socket.on('updateCards', (data) => {
-        gameState.homeReds = data.homeReds;
-        gameState.awayReds = data.awayReds;
-        io.emit('stateUpdate', gameState);
-    });
-
-    // Puente de comunicación para los Cambios / Sustituciones
-    socket.on('triggerSubstitution', (data) => {
-        io.emit('showSubstitution', data);
-    });
-
-    socket.on('timerControl', (command) => {
-        if (command.action === 'start') startTimer();
-        else if (command.action === 'pause') stopTimer();
-        else if (command.action === 'reset') {
-            stopTimer();
-            gameState.minutes = 0;
-            gameState.seconds = 0;
-            gameState.addedTime = 0;
-            io.emit('updateTimer', { minutes: 0, seconds: 0 });
-        } else if (command.action === 'set') {
-            gameState.minutes = parseInt(command.minutes) || 0;
-            gameState.seconds = 0;
-            io.emit('updateTimer', { minutes: gameState.minutes, seconds: gameState.seconds });
-        }
+    socket.on('toggleScoreboard', (v) => { gameState.showScoreboard = v; io.emit('stateUpdate', gameState); });
+    socket.on('updateScore', (d) => { gameState.homeScore = d.homeScore; gameState.awayScore = d.awayScore; io.emit('stateUpdate', gameState); });
+    socket.on('updateTeamInfo', (d) => { gameState = { ...gameState, ...d }; io.emit('stateUpdate', gameState); });
+    socket.on('updateAddedTime', (t) => { gameState.addedTime = t; io.emit('stateUpdate', gameState); });
+    socket.on('updateCards', (d) => { gameState.homeReds = d.homeReds; gameState.awayReds = d.awayReds; io.emit('stateUpdate', gameState); });
+    socket.on('triggerSubstitution', (d) => { io.emit('showSubstitution', d); });
+    socket.on('timerControl', (c) => {
+        if (c.action === 'start') startTimer();
+        else if (c.action === 'pause') stopTimer();
+        else if (c.action === 'reset') { stopTimer(); gameState.minutes = 0; gameState.seconds = 0; gameState.addedTime = 0; io.emit('updateTimer', { minutes: 0, seconds: 0 }); }
+        else if (c.action === 'set') { gameState.minutes = parseInt(c.minutes) || 0; gameState.seconds = 0; io.emit('updateTimer', { minutes: gameState.minutes, seconds: gameState.seconds }); }
         io.emit('stateUpdate', gameState);
     });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Servidor de gráficos corriendo en http://localhost:${PORT}`);
-});
+server.listen(process.env.PORT || 3000, () => console.log('Servidor en línea'));
